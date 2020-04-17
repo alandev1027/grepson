d355f1c7d366f3f32921ec4a291ef999c84ea57e
Alan <alandev1027@gmail.com>
2020-04-17 12:47:19 +0300

    changed ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Row, Col, Typography, Progress } from 'antd';
+import { Row, Col, Typography, Menu } from 'antd';
 import "./Header.scss";
+import { NavLink } from "react-router-dom";
+const { Title } = Typography;
 
 export default function Header() {
   return (
     <Row className="header-container">
-      Header
+      <Col xs={24} sm={24} md={6}>
+        <Title className="logo-title">Grepson.</Title>
+      </Col>
+      <Col xs={24} sm={24} md={18}>
+        <Row justify="center">
+          <Menu mode="horizontal">
+            <Menu.Item key="about_us">
+              <NavLink to="/">ABOUT US</NavLink>
+            </Menu.Item>
+            <Menu.Item key="impact">
+              <NavLink to="/">IMPACT</NavLink>
+            </Menu.Item>
+            <Menu.Item key="services">
+              <NavLink to="/">SERVICES</NavLink>
+            </Menu.Item>
+            <Menu.Item key="insights">
+              <NavLink to="/">INSIGHTS</NavLink>
+            </Menu.Item>
+            <Menu.Item key="get_in_touch">
+              <NavLink to="/">GET IN TOUCH</NavLink>
+            </Menu.Item>
+            <Menu.Item key="menu">
+              <NavLink to="/">MENU</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Row>
+      </Col>
     </Row>
   )
 }
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Row} from 'antd';
+import { Row, Typography, Col } from 'antd';
 import "./Main.scss";
+import LogoImage from "../../assets/img_logo.png";
+import WomanImage from "../../assets/img_woman.png";
+const { Title, Text } = Typography;
 
 export default function Main() {
   return (
     <Row className="main-container">
-      MainPage
+      <Col span={24}>
+        <Row>
+          <Col span={22} offset={2} className="woman-img-col">
+            <div className="woman-img" />
+          </Col>
+        </Row>
+        <Row className="body-container" align="bottom">
+          <Col span={24}>
+            <Row>
+              <Col span={20} offset={2}>
+                <Row>
+                  <Title className="title">real impact</Title>
+                </Row>
+                <Row>
+                  <Text className="text">By partenirng only with top wine and champagne companies that <br />want to disrupt their niche and be a benchmark.</Text>
+                </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={20} offset={2} className="logo-col">
+                <Row justify="end">
+                  <img src={LogoImage} alt="logo" className="logo-img" />
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
     </Row>
   )
 }
